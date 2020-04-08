13eb9ceb6a0b5631b1587e5d106a900593b2e743
emsbn <emsbn1@gmail.com>
2020-04-08 22:01:06 +0900

    post /api/post

--- ch5/back/routes/post.js
+++ ch5/back/routes/post.js
@@ -1,8 +1,45 @@
 const express = require('express');
+const db = require('../models');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {});
+router.post('/', async (req, res, next) => {
+  try {
+    const hashtags = req.body.content.match(/#[^\s]+/g);
+    const newPost = await db.post.create({
+      content: req.body.content, // ex) '제로초 파이팅 #구독 #좋아요 눌러주세요'
+      UserId: req.user.id,
+    });
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map(tag =>
+          db.Hashtag.findOrCreate({
+            where: { name: tag.slice(1).toLowerCase() },
+          })
+        )
+      );
+      console.log(result);
+      await newPost.addHashtags(result.map(r => r[0]));
+    }
+
+    // const postUser = await newPost.getUser();
+    // newPost.User = postUser;
+    // res.json(newPost);
+    const fullPost = await db.Post.findOne({
+      where: { id: newPost.id },
+      include: [
+        {
+          model: db.User,
+        },
+      ],
+    });
+    res.json(fullPost);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
 router.post('/images', (req, res) => {});
 
 module.exports = router;
